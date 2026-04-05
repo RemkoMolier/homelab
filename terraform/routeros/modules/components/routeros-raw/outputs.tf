232a032238214f869146382c992181a415f1f5af
Remko Molier <remko.molier@googlemail.com>
2026-04-05 23:06:38 +0200

    chore(infra): remove routeros-raw escape hatch module
    Replaced by native provider resources and SSH provisioners.

--- terraform/routeros/modules/components/routeros-raw/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  description = "The RouterOS .id of the managed resource"
-  value       = restapi_object.this.id
-}
-
-output "api_data" {
-  description = "The full API response data"
-  value       = restapi_object.this.api_data
-}
